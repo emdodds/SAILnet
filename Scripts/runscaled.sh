bc6721fd4ee1101dcf7cd2e04f86ada9a6341ba2
Eric Dodds <edodds828@gmail.com>
2017-04-27 00:03:59 -0700

    script for running scaled version on cluster

---
+++ Scripts/runscaled.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Partition:
+#SBATCH --partition=cortex
+#
+# Wall clock limit:
+#SBATCH --time=48:00:00
+#
+# Memory:
+#SBATCH --mem-per-cpu=8G
+
+cd /global/home/users/edodds/SAILnet/Scripts
+python trainscaled.py 
