f5b006d045e08756c048fa4782c2f96e78079a10
Eric Dodds <edodds828@gmail.com>
2017-05-12 14:19:56 -0700

    fixed gain

--- Scripts/trainscaled.py
+++ Scripts/trainscaled.py
@@ -17,8 +17,17 @@ parser.set_defaults(scaled=True)
 parser.set_defaults(load=False)
 args = parser.parse_args()
 datatype = args.data
-Net = SAILmods.VarTimeSAILnet if args.scaled else SAILnet.SAILnet
-prefix = 'scaled' if args.scaled else ''
+
+kwargs = {'p': 0.01,
+          'theta0': 2.5}
+if args.scaled:
+    Net = SAILmods.VarTimeSAILnet
+    prefix = 'scaled'
+    kwargs['gain_rate'] = 0.0
+    kwargs['gain'] = 2.0
+else:
+    Ne = SAILnet.SAILnet
+    prefix = ''
 
 if datatype == 'images':
     oc = 8
@@ -26,8 +35,9 @@ if datatype == 'images':
     wholeims /= wholeims.std()
     numinput = 256
     numunits = numinput*oc
-    net = Net(data=wholeims, nunits=numunits, theta0=2.0,
-              paramfile=prefix+'SAIL_bvh_8oc.pickle')
+    net = Net(data=wholeims, nunits=numunits,
+              paramfile=prefix+'SAIL_bvh_8oc.pickle',
+              **kwargs)
 elif datatype == 'pcaimages':
     oc = 10
     datafile = '../../vision/Data/300kvanHateren'
@@ -37,11 +47,12 @@ elif datatype == 'pcaimages':
     data = np.load(datafile+'200.npy')
     data = data/data.std()
     numunits = numinput*oc
-    net = Net(data=data, nunits=numunits, theta0=2.0,
+    net = Net(data=data, nunits=numunits,
               datatype='image',
               pca=mypca,
               stimshape=origshape, ninput=numinput,
-              paramfile=prefix+'SAIL_pcavh_10oc.pickle')
+              paramfile=prefix+'SAIL_pcavh_10oc.pickle',
+              **kwargs)
 
 elif datatype == 'spectro':
     oc = 10
@@ -52,21 +63,17 @@ elif datatype == 'spectro':
     data = np.load(datafile+'.npy')
     data = data/data.std()
     numunits = numinput*oc
-    net = Net(data=data, nunits=numunits, theta0=2.0,
+    net = Net(data=data, nunits=numunits,
               datatype='spectro',
               pca=mypca,
               stimshape=origshape, ninput=numinput,
-              paramfile=prefix+'scaledSAIL_allTIMIT_10oc.pickle')
+              paramfile=prefix+'scaledSAIL_allTIMIT_10oc.pickle',
+              **kwargs)
 
 if args.load:
     net.load(net.paramfile)
 else:
     net.set_dot_inhib()
-    net.p = 0.01
-    net.beta = 0.0
-    net.run(2000)
-    net.beta = 0.001
-    net.save()
 
 net.run(10000)
 net.save()
